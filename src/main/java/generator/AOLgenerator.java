bf0d72def03befeaf4e36ff252ad2b95a03fcc44
Swrrt <swrrt.11@gmail.com>
2019-01-27 18:45:06 +0800

    Kafka Producer

---
+++ src/main/java/generator/AOLgenerator.java
@@ -0,0 +1,18 @@
+package generator;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+    Reading AOL search data (AOL_search_data_leak_2006.zip) and send them to Kafka topic.
+    Could start multiple generator on different host.
+*/
+public class AOLgenerator {
+    private final String outputTopic = "StreamBenchInput";
+    public static void main(String[] arg){
+        KafkaProducer a = ;
+
+    }
+}
