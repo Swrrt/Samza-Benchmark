f823af11509c3c9f504943d5449ca8e5b944ecdd
unknown <workshop@soccf-snr4-004.comp.nus.edu.sg>
2019-02-21 19:06:55 +0800

    run the jar with dependencies

--- src/scripts/generate-AOL.sh
+++ src/scripts/generate-AOL.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-mvn exec:java -Dexec.mainClass="generator.AOLgenerator" -Dexec.args="/home/samza/dataset/AOL-user-ct-collection/user-ct-test-collection-01.txt"
+#mvn exec:java -Dexec.mainClass="generator.AOLgenerator" -Dexec.args="/home/samza/dataset/AOL-user-ct-collection/user-ct-test-collection-01.txt"
+java -cp target/benchmark-0.0.1-jar-with-dependencies.jar generator.AOLgenerator /home/samza/dataset/AOL-user-ct-collection/user-ct-test-collection-01.txt
