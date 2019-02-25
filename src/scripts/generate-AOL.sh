800298b0c86fc61a87bf057befb045eb64e13827
unknown <workshop@soccf-snr4-004.comp.nus.edu.sg>
2019-02-25 15:15:22 +0800

    Testing

--- src/scripts/generate-AOL.sh
+++ src/scripts/generate-AOL.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 #mvn exec:java -Dexec.mainClass="generator.AOLgenerator" -Dexec.args="/home/samza/dataset/AOL-user-ct-collection/user-ct-test-collection-01.txt"
-java -cp target/benchmark-0.0.1-jar-with-dependencies.jar generator.AOLgenerator /home/samza/dataset/AOL-user-ct-collection/user-ct-test-collection-01.txt
+#java -cp target/benchmark-0.0.1-jar-with-dependencies.jar generator.AOLgenerator /home/samza/dataset/AOL-user-ct-collection/user-ct-test-collection-01.txt
+java -cp target/benchmark-0.0.1-jar-with-dependencies.jar generator.AOLgenerator /home/samza/dataset/AOL-user-ct-collection/user-ct-test-collection-01.txt StreamBenchInput yy04:9092,yy05:9093,yy06:9094,yy07:9095,yy08:9096
