8e7f4f2be753f0728cdec7ce88303e6c5e7eec64
unknown <workshop@soccf-snr4-004.comp.nus.edu.sg>
2019-03-07 19:50:41 +0800

    Add scripts to remove-topics

--- src/scripts/remove-topic.sh
+++ src/scripts/remove-topic.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 # ./remove-topic.sh TOPICNAME
+# Manually:
+# Stop kafka server
+# Remove topics in zookeeper: /brokers/topics/TOPICNAME
+# Connect to brokers, rm -rf /tmp/kafka-logs/MyTopics-
+# Start kafka
+
+
+# Add config "delete.topic.enable=true" to server.properties
+
+kafka/bin/kafka-topics --delete --zookeeper yy04:2181 --topic $1
+
 #
-# Remember to remove topics in zookeeper: /brokers/topics/TOPICNAME
+# Temporarily update the retention time to one second
 #
-./bin/kafka-topics --delete --zookeeper yy04:2181 --topic $1
+kafka/bin/kafka-topics.sh --zookeeper yy04:2181 --alter --topic $1 --config retention.ms=1000
+
+# Restore retention
+# kafka/bin/kafka-topics.sh --zookeeper yy04:2181 --alter --topic $1 --deleteConfig retention.ms
