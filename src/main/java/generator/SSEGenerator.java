43b3d25993afaed023ae03c26280693c45b211f5
Mao Yancan <maoyancan@u.nus.edu>
2019-10-26 19:58:33 +0800

    Update SSEGenerator.java
    reset to old history

--- src/main/java/generator/SSEGenerator.java
+++ src/main/java/generator/SSEGenerator.java
@@ -4,12 +4,15 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
 import java.io.*;
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.*;
+import org.apache.log4j.Logger;
 
 /**
  * SSE generaor
  */
-public class SSEGenerator {
+class SSEGnerator {
 
     private String TOPIC;
 
@@ -22,11 +25,11 @@ public class SSEGenerator {
     private static final int Order_Vol = 10;
     private static final int Sec_Code = 11;
     private static final int Trade_Dir = 22;
-
-    public SSEGenerator(String input) {
+    private static final int Last_Upd_Time = 3;
+    public SSEGnerator(String input, String bootstrapServer) {
         TOPIC = input;
         Properties props = new Properties();
-        props.put("bootstrap.servers", "kafka-server-1:9092");
+        props.put("bootstrap.servers", bootstrapServer);
         props.put("client.id", "ProducerExample");
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
@@ -35,7 +38,7 @@ public class SSEGenerator {
 
     }
 
-    public void generate(String file, int speed) throws InterruptedException {
+    public void generate(String file, int speed, long startPoint) throws InterruptedException {
 
         String sCurrentLine;
         List<String> textList = new ArrayList<>();
@@ -48,32 +51,48 @@ public class SSEGenerator {
         long interval = 0;
         int counter = 0;
         try {
-            stream = new FileReader("/home/myc/workspace_backup/"+file+".txt");
+            stream = new FileReader(file);
             br = new BufferedReader(stream);
-
-            interval = 1000000000/speed;
-            start = System.nanoTime();
-
+//            Thread.sleep(10000);
+            System.out.println("Start point: " + startPoint);
+            boolean isStart = false;
             while ((sCurrentLine = br.readLine()) != null) {
-
-                cur = System.nanoTime();
-                if (sCurrentLine.equals("end")) {
-                    continue;
+                long time = 0;
+                if(sCurrentLine.split("\\|").length >= 10) {
+                    String t = sCurrentLine.split("\\|")[Last_Upd_Time];
+                    time = Duration.between(LocalTime.MIN, LocalTime.parse(t)).toMillis() / 1000;
                 }
-
-                if (sCurrentLine.split("\\|").length < 10) {
-                    continue;
-                }
-
-                ProducerRecord<String, String> newRecord = new ProducerRecord<>(TOPIC, sCurrentLine.split("\\|")[Sec_Code], sCurrentLine);
-                producer.send(newRecord);
-                counter++;
-
-                while ((System.nanoTime() - cur) < interval) {}
-                if (System.nanoTime() - start >= 1000000000) {
-                    System.out.println("output rate: " + counter);
-                    counter = 0;
-                    start = System.nanoTime();
+                if(time > startPoint)isStart = true;
+                if(isStart) {
+                    if (sCurrentLine.equals("end")) {
+                        counter++;
+                    }
+                    if (counter == speed) {
+                        start = System.nanoTime();
+                        interval = 1000000000 / textList.size();
+                        for (int i = 0; i < textList.size(); i++) {
+                            cur = System.nanoTime();
+                            if (textList.get(i).split("\\|").length < 10) {
+                                continue;
+                            }
+                            StringBuilder key = new StringBuilder(textList.get(i).split("\\|")[Sec_Code]);
+                            //key.append("|");
+                            //key.append(cur);
+                            ProducerRecord<String, String> newRecord = new ProducerRecord<>(TOPIC, key.toString(), textList.get(i));
+                            producer.send(newRecord);
+                            producer.flush();
+                            while ((System.nanoTime() - cur) < interval) {
+                            }
+                        }
+                        //}
+                        System.out.println("size:" + String.valueOf(textList.size()));
+                        System.out.println("time:" + String.valueOf((System.nanoTime() - start) / 1000000));
+                        System.out.println("interval:" + String.valueOf((System.nanoTime() - start) / (textList.size())));
+                        textList.clear();
+                        counter = 0;
+                        continue;
+                    }
+                    textList.add(sCurrentLine);
                 }
             }
         } catch (IOException e) {
@@ -90,16 +109,84 @@ public class SSEGenerator {
         //logger.info("LatencyLog: " + String.valueOf(System.currentTimeMillis() - time));
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    public void warmup(String file, int speed, long period) throws IOException {
+        String sCurrentLine;
+        List<String> textList = new ArrayList<>();
+        FileReader stream = null;
+        // // for loop to generate message
+        BufferedReader br = null;
+        int sent_sentences = 0;
+        long cur = 0;
+        long start = 0;
+        long interval = 0;
+        int counter = 0;
+        long begin = System.currentTimeMillis();
+        try {
+            System.out.println("warm up start...");
+            stream = new FileReader(file);
+            br = new BufferedReader(stream);
+            interval = 1000000000 / speed;
+            start = System.nanoTime();
+
+            while ((sCurrentLine = br.readLine()) != null) {
+                if (System.currentTimeMillis() - begin < period) {
+
+                    cur = System.nanoTime();
+                    if (sCurrentLine.equals("end")) {
+                        continue;
+                    }
+
+                    if (sCurrentLine.split("\\|").length < 10) {
+                        continue;
+                    }
+
+                    ProducerRecord<String, String> newRecord = new ProducerRecord<>(TOPIC, sCurrentLine.split("\\|")[Sec_Code], sCurrentLine);
+                    producer.send(newRecord);
+                    counter++;
+
+                    while ((System.nanoTime() - cur) < interval) {
+                    }
+                    if (System.nanoTime() - start >= 1000000000) {
+                        System.out.println("output rate: " + counter);
+                        counter = 0;
+                        start = System.nanoTime();
+                    }
+                } else {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(stream != null) stream.close();
+                if(br != null) br.close();
+            } catch(IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        producer.close();
+    }
+
+    public static void main(String[] args) throws InterruptedException, IOException {
         String TOPIC = new String("stock");
         String file = new String("partition1");
         int speed = 1;
+        long startPoint = 0;  //In second
+        long warmupPeriod = 10000;
+        int warmupSpeed = 20;
+        String bootstrapServer = "localhost:9092";
         if (args.length > 0) {
             TOPIC = args[0];
             file = args[1];
             speed = Integer.parseInt(args[2]);
+            startPoint = Long.parseLong(args[3]);
+            warmupSpeed = Integer.parseInt(args[4]);
+            warmupPeriod = Long.parseLong(args[5]);
+            bootstrapServer = args[6];
         }
-        new SSEGenerator(TOPIC).generate(file, speed);
+        SSEGnerator generator = new SSEGnerator(TOPIC, bootstrapServer);
+        generator.warmup(file, warmupSpeed, warmupPeriod); //period(ms)
+        generator.generate(file, speed, startPoint);
     }
 }
-
