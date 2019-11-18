955792aa03a17d761e83da6dba3ee66441439785
maoyancan <maoyancan@gmail.com>
2019-11-18 11:50:11 +0800

    add stock window

---
+++ src/main/java/samzaapp/stock/StockWindow.java
@@ -0,0 +1,88 @@
+package samzaapp.stock;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import joptsimple.OptionSet;
+import org.apache.samza.application.StreamApplication;
+import org.apache.samza.application.descriptors.StreamApplicationDescriptor;
+import org.apache.samza.config.Config;
+import org.apache.samza.operators.KV;
+import org.apache.samza.operators.MessageStream;
+import org.apache.samza.operators.OutputStream;
+import org.apache.samza.operators.windows.Windows;
+import org.apache.samza.runtime.LocalApplicationRunner;
+import org.apache.samza.serializers.IntegerSerde;
+import org.apache.samza.serializers.KVSerde;
+import org.apache.samza.serializers.Serde;
+import org.apache.samza.serializers.StringSerde;
+import org.apache.samza.system.kafka.descriptors.KafkaInputDescriptor;
+import org.apache.samza.system.kafka.descriptors.KafkaOutputDescriptor;
+import org.apache.samza.system.kafka.descriptors.KafkaSystemDescriptor;
+import org.apache.samza.util.CommandLine;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+
+public class StockWindow implements StreamApplication {
+    private static final int Order_No = 0;
+    private static final int Tran_Maint_Code = 1;
+    private static final int Order_Price = 8;
+    private static final int Order_Exec_Vol = 9;
+    private static final int Order_Vol = 10;
+    private static final int Sec_Code = 11;
+    private static final int Trade_Dir = 22;
+
+
+    private static final String KAFKA_SYSTEM_NAME = "kafka";
+    private static final List<String> KAFKA_CONSUMER_ZK_CONNECT = ImmutableList.of("localhost:2181");
+    private static final List<String> KAFKA_PRODUCER_BOOTSTRAP_SERVERS = ImmutableList.of("localhost:9092");
+    private static final Map<String, String> KAFKA_DEFAULT_STREAM_CONFIGS = ImmutableMap.of("replication.factor", "1");
+
+    private static final String INPUT_STREAM_ID = "stock_order";
+    private static final String OUTPUT_STREAM_ID = "stock_price";
+
+
+    @Override
+    public void describe(StreamApplicationDescriptor streamApplicationDescriptor) {
+        Serde serde = KVSerde.of(new StringSerde(), new StringSerde());
+
+        KafkaSystemDescriptor kafkaSystemDescriptor = new KafkaSystemDescriptor(KAFKA_SYSTEM_NAME)
+            .withConsumerZkConnect(KAFKA_CONSUMER_ZK_CONNECT)
+            .withProducerBootstrapServers(KAFKA_PRODUCER_BOOTSTRAP_SERVERS)
+            .withDefaultStreamConfigs(KAFKA_DEFAULT_STREAM_CONFIGS);
+
+        KafkaInputDescriptor<KV<String, String>> inputDescriptor =
+                kafkaSystemDescriptor.getInputDescriptor(INPUT_STREAM_ID,
+                        serde);
+
+        KafkaOutputDescriptor<KV<String, String>> outputDescriptor =
+                kafkaSystemDescriptor.getOutputDescriptor(OUTPUT_STREAM_ID,
+                        serde);
+
+        MessageStream<KV<String, String>> inputStream = streamApplicationDescriptor.getInputStream(inputDescriptor);
+        OutputStream<KV<String, String>> outputStream = streamApplicationDescriptor.getOutputStream(outputDescriptor);
+
+        inputStream
+            .map(order -> {
+                String[] orderArr = order.getValue().split("\\|");
+                return new KV(order.getKey(), orderArr[Sec_Code]);
+            })
+            .window(Windows.keyedTumblingWindow(kv -> String.valueOf(kv.getValue()),
+                    Duration.ofSeconds(5), () -> 0, (m, prevCount) -> prevCount + 1,
+                    new StringSerde(), new IntegerSerde()), "count")
+            .map(windowPane ->
+                    KV.of(windowPane.getKey().getKey(),
+                            windowPane.getKey().getKey() + ": " + windowPane.getMessage().toString()))
+            .sendTo(outputStream);
+    }
+
+    public static void main(String[] args) {
+        CommandLine cmdLine = new CommandLine();
+        OptionSet options = cmdLine.parser().parse(args);
+        Config config = cmdLine.loadConfig(options);
+        LocalApplicationRunner runner = new LocalApplicationRunner(new StockWindow(), config);
+        runner.run();
+        runner.waitForFinish();
+    }
+}
